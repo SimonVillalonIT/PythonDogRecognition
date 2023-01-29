d7b4338c290cde9ef6014fd170a203e19bc54239
SimonVillalonIT <simonvillallon10@gmail.com>
2023-01-29 17:34:52 -0300

    API working fine

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 from DogDetector import dog_detector 
-from pydantic import BaseModel
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-origins = [
-    "http://0.0.0.0:3000",
-]
-
-
+from fastapi import FastAPI, File, UploadFile, Form
+import os
+import time
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get('/')
 def read_root():
     return "Hello World!"
 
-@app.get('/detect')
-def get_detect():
-    return {"isadog": dog_detector("/home/simonvillalon/Downloads/1.jpg")}
+@app.post('/detect')
+async def post_detect(file: UploadFile = File(...)):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filename = f'{dir_path}/uploads/{time.time()}-{file.filename}'
+    f = open(f'{filename}', 'wb')
+    content = await file.read()
+    f.write(content)
+    result = {"Result": dog_detector(filename)}
+    os.remove(filename)
+    return result
